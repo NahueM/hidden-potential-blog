96a9a15847ecc063d139794ab0551f0e7d7500c4
NahueM <lnahuelmaya@gmail.com>
2023-07-02 10:31:22 +0200

    correct revalidate time

--- app/[lang]/posts/[language]/[postId]/page.tsx
+++ app/[lang]/posts/[language]/[postId]/page.tsx
@@ -4,7 +4,7 @@ import { notFound } from "next/navigation"
 
 import 'highlight.js/styles/github-dark.css'
 
-export const revalidate = 10;
+export const revalidate = 86400;
 
 type Props = {
     params: {
