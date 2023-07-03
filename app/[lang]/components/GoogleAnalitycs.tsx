98baa54aa50a027408baea45e2cf7a4f7d2580c5
NahueM <lnahuelmaya@gmail.com>
2023-07-03 11:36:15 +0200

    google analitycs

---
+++ app/[lang]/components/GoogleAnalitycs.tsx
@@ -0,0 +1,20 @@
+const GoogleAnalytics = ({ ga_id }:{ ga_id: string}) => (
+    <>
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${ga_id}`}
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+      
+            gtag('config', '${ga_id}');
+          `,
+        }}
+      ></script>
+    </>
+  );
+  export default GoogleAnalytics;
--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -2,6 +2,7 @@
 import './globals.css'
 import Navbar from './components/Navbar';
 import { i18n } from '../../i18n-config'
+import GoogleAnalytics from './components/GoogleAnalitycs';
 
 export async function generateStaticParams() {
   return i18n.locales.map((locale) => ({ lang: locale }))
@@ -18,6 +19,9 @@ export default function RootLayout({
 return (
   <html lang={params.lang}>
     <body className="dark:bg-slate-800">
+      {process.env.GOOGLE_ANALYTICS_ID ? (
+      <GoogleAnalytics ga_id={process.env.GOOGLE_ANALYTICS_ID} />
+        ) : null}
         <Navbar />
         <main className="flex justify-center px-4 md:px-6 prose max-w-7xl prose-slate dark:prose-invert mx-auto">
           {children}
